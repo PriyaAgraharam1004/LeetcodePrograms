fae79809f7ce7b54bfc1c4d42c7cec828a310683
PriyaAgraharam1004 <lakshmima2002@gmail.com>
2026-09-02 20:14:34 +0530

    Time: 0 ms (100%), Space: 42.8 MB (71.58%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
@@ -0,0 +1,15 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        Set<Character> st=new HashSet<>();
+        for(char c:s.toCharArray()){
+            if(st.contains(c)){
+                return c;
+            }
+            else{
+                st.add(c);
+            }
+        }
+        return ' ';
+        
+    }
+}
